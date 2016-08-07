e7edbcb9a93c3928d9a57e23b1a7c80fe397a3ab
xinghua2201 <xinghua2201@139.com>
2016-08-07 17:09:06 +0800

    git remote add origin https://github.com/xinghua2201/Demo.git

---
+++ src/SayHello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author root
+ *
+ */
+public class SayHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello,git");
+	}	
+
+}
